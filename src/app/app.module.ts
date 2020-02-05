f261af3e5464bb8298c732ab71a3a9c897a282d5
=luan_caracanha <luan.caracanha@gmail.com>
2020-02-05 01:21:20 -0300

    adicionando primeng como dependencia do projeto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenubarModule } from 'primeng/menubar';
+import {AccordionModule} from 'primeng/accordion';
+import { InputTextModule } from 'primeng/inputtext';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    AppRoutingModule,
+    MenubarModule,
+    AccordionModule,
+    InputTextModule
   ],
   providers: [],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
